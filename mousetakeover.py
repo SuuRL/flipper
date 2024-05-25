4816c884208a165bab465d0a0dc8d2f801d69add
SuuRL <105046972+SuuRL@users.noreply.github.com>
2024-05-25 06:54:14 +0200

    Update mousetakeover.py

--- mousetakeover.py
+++ mousetakeover.py
@@ -6,10 +6,8 @@ except:
     import pyautogui
 import random
 import time
-
 while True:
-    # Move the mouse cursor randomly
+    time.sleep(0.1)
     x_offset = random.randint(-250, 250)
     y_offset = random.randint(-250, 250)
     pyautogui.moveRel(x_offset, y_offset, duration=0.25)
-    time.sleep(0.1)  # 1 second delay
