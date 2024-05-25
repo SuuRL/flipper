527a8872cc3bbdb400c17b34e9a931da00d75e0f
SuuRL <105046972+SuuRL@users.noreply.github.com>
2024-05-25 06:56:08 +0200

    Update mousetakeover.py
    offsets cursor randomly

--- mousetakeover.py
+++ mousetakeover.py
@@ -8,6 +8,4 @@ import random
 import time
 while True:
     time.sleep(0.1)
-    x_offset = random.randint(-250, 250)
-    y_offset = random.randint(-250, 250)
-    pyautogui.moveRel(x_offset, y_offset, duration=0.25)
+    pyautogui.moveRel(random.randint(-250, 250), random.randint(-250, 250), duration=0.25)
